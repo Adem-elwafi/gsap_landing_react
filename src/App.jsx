454a6c2b65032d3be13c0bb0b836b03837394f94
Adem-elwafi <adem.elwafi.dev@gmail.com>
2025-12-15 21:01:12 +0100

    feat: create Hero.jsx with GSAP SplitText and scroll animations

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import {ScrollTrigger,SplitText} from "gsap/all";
 import gsap from 'gsap';
 import './index.css';
 import Navbar from './components/Navbar.jsx';
+import Hero from './components/Hero.jsx';
 
 gsap.registerPlugin(ScrollTrigger, SplitText); 
 const App = () => {
   return (
     <main>
       <Navbar/>
+      <Hero/> 
+      <div className="h-dvh bg-black"></div>
     </main>
   )
 }
